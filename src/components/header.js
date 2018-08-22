e0897c48398e46b004e624acc344eb1f3c02b41e
blermyjake <jeremy73120@gmail.com>
2018-08-21 21:40:22 -0500

    some style

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './header.css'
+import logo from '../images/empire.png'
+import logo2 from '../images/oldRepublic.png'
+
+// console.log(logo)
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'maroon',
       marginBottom: '1.45rem',
     }}
   >
@@ -23,10 +28,28 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-          {siteTitle}
+          {/* {siteTitle} */}
+          <div className="logoHeader">
+            <img src={logo} alt="Empire logo" />
+            <img src={logo2} alt="Empire logo" />
+            <img src={logo} alt="Empire logo" />
+            <img src={logo2} alt="Empire logo" />
+            <img src={logo} alt="Empire logo" />
+            <img src={logo2} alt="Empire logo" />
+          </div>
         </Link>
       </h1>
     </div>
+    <nav className="navLink">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 )
 
---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class about extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us </h1>
+        <p>
+          Lorem ipsum dolor amet gochujang vaporware retro tumblr 90's blog
+          meggings distillery. Roof party post-ironic chartreuse, hoodie raw
+          denim freegan organic affogato vice tousled pabst shaman distillery
+          succulents. Listicle church-key deep v shoreditch skateboard
+          stumptown. 3 wolf moon pork belly schlitz woke. Lo-fi live-edge woke
+          ramps knausgaard leggings chillwave lomo.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default about
